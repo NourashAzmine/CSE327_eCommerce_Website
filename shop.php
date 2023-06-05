c6560dd844868eaaa987461788f345cc5ef0e964
Nourash Azmine Chowdhury <64426212+NourashAzmine@users.noreply.github.com>
2023-06-05 20:30:40 +0600

    sixth commit
    made unable to add products in the cart without login

--- shop.php
+++ shop.php
@@ -12,23 +12,28 @@ if(isset($_SESSION['user_id'])){
 }
  
 
-if(isset($_POST['add_to_cart'])){
-
+if (isset($_POST['add_to_cart'])) {
    $product_name = $_POST['product_name'];
    $product_price = $_POST['product_price'];
    $discount_product_price = $_POST['discount_product_price'];
    $product_image = $_POST['product_image'];
    $product_quantity = $_POST['product_quantity'];
 
-   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+   // Check if the user is logged in
+   if ($user_id != '') {
+      $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
 
-   if(mysqli_num_rows($check_cart_numbers) > 0){
-      $message[] = 'already added to cart!';
-   }else{
-      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price,discount_price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$discount_product_price','$product_quantity', '$product_image')") or die('query failed');
-      $message[] = 'product added to cart!';
+      if (mysqli_num_rows($check_cart_numbers) > 0) {
+         $message[] = 'already added to cart!';
+      } else {
+         mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price,discount_price, quantity, image) VALUES('$user_id', '$product_name', '$product_price','$discount_product_price', '$product_quantity', '$product_image')") or die('query failed');
+         $message[] = 'product added to cart!';
+      }
+   } else {
+      // Redirect the user to the login page
+      header("Location: login.php");
+      exit();
    }
-
 }
 
 ?>
